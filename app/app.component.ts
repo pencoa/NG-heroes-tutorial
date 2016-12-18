b7f34a796fdd74a69f56036b049ffad421af4db4
Randy Pen <orz@f2cafe.com>
2016-12-18 22:22:50 +0800

    路由第一次修改

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,16 +6,7 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    <my-heroes></my-heroes>
   `,
   styles: [`
     .selected {
@@ -68,24 +59,9 @@ import { HeroService } from './hero.service';
   `],
   providers: [HeroService]
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
 
 
