ab5fb19082ae43a9e694f37e9d44c31664747603
Randy Pen <orz@f2cafe.com>
2016-12-23 00:55:04 +0800

    更新 HeroesComponent 类

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }   from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -11,17 +12,27 @@ import { HeroService } from './hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
+
