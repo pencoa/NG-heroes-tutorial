9362d95264c46281610be377600ffb9f22da663e
Randy Pen <orz@f2cafe.com>
2016-12-24 00:20:34 +0800

    import HttpModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
@@ -15,6 +16,7 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
@@ -23,9 +25,7 @@ import { AppRoutingModule }     from './app-routing.module';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
