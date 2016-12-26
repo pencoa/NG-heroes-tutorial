180ccf9e3882b248388d1018edf6520fce46e1eb
Randy Pen <orz@f2cafe.com>
2016-12-26 19:58:12 +0800

    search

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,6 +14,9 @@ import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
+
+import './rxjs-extensions';
 
 @NgModule({
   imports: [
@@ -28,6 +31,7 @@ import { HeroService }          from './hero.service';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent,
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
